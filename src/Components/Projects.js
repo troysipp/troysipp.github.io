df95cc71eb7fc7c34e171a15282700afe0dfeabb
troysipp <sipprelle_troy@bah.com>
2018-02-02 13:18:20 -0500

    continue styling and add project links

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import "./Projects.css";
 
 class Projects extends Component {
   render() {
     return (
       <div className="project-list">
-        <p className="project">Tower of Hanoi</p>
-        <p className="project">recipeat</p>
-        <p className="project">Home Finder</p>
-        <p className="project">Just for Friends</p>
+        <div className="project">
+          <a href="https://troysipp.github.io/tower-of-hanoi/">
+            Tower of Hanoi
+          </a>
+        </div>
+        <div className="project">
+          <a href="https://recipe-eat.herokuapp.com/recipes">recipeat</a>
+        </div>
+        <div className="project">
+          <a href="http://home-finder.surge.sh/homes">Home Finder</a>
+        </div>
+        <div className="project">
+          <a href="http://social-network.surge.sh/home">JustForFriends</a>
+        </div>
       </div>
     );
   }
