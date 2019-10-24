f59f3a3465e4f8a6d924b748584182153085e2b4
594037 <troysipprelle@C02V40ZGHTDG.local>
2019-10-24 11:57:25 -0400

    add some text back into html

--- src/Components/App.js
+++ src/Components/App.js
@@ -14,37 +14,7 @@
 //   render() {
 //     return (
 //       <div className="App">
-//         <header className="App-header">
-//           <h1 className="App-title">
-//             <Link to="/">Troy Sipprelle</Link>
-//           </h1>
-//         </header>
-//         <div className="body">
-//           <nav>
-//             {/* <p>Want to know...</p> */}
-//             <Link to="/about">More about me</Link>
-//             <Link to="/projects">What I've been up to lately</Link>
-//           </nav>
-//           <div className="main">
-//             <Switch>
-//               <Route exact path="/about" render={() => <About />} />
-//               <Route exact path="/projects" render={() => <Projects />} />
-//               <Route path="/*" render={() => <Redirect to="/" />} />
-//             </Switch>
-//           </div>
-//           <img src={me} className="me-pic" />
-//         </div>
-//         <footer>
-//           <a
-//             className="footer-link"
-//             href="https://www.linkedin.com/in/troy-sipprelle-516b1491/"
-//           >
-//             LinkedIn
-//           </a>
-//           <a className="footer-link" href="https://github.com/troysipp">
-//             GitHub
-//           </a>
-//         </footer>
+//         
 //       </div>
 //     );
 //   }
