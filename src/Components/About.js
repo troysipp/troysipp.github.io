8e367e0f0ed83bbc8f72870c0a29e0d485ca6944
594037 <troysipprelle@C02V40ZGHTDG.local>
2019-10-24 11:51:46 -0400

    remove react

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,29 +1,29 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-import "./About.css";
+// import "./About.css";
 
-class About extends Component {
-  render() {
-    return (
-      <div>
-        <p>My name is Troy Conner Sipprelle.</p>
-        <p>
-          I am a Software Engineer for Booz Allen Hamilton, and I live in the
-          Washington, D.C. Metro Area. I like eating, cooking, watching movies,
-          and playing the ukulele.
-        </p>
-        <p>
-          I graduated from Williams College in the spring of 2017 with a Math
-          major and a focus on Architecture. Since then, I've learned to code
-          and am excited about building both physical and less-tangible things
-          that make people happy.
-        </p>
-        <p>
-          Check out my: <a href="##">resume</a>
-        </p>
-      </div>
-    );
-  }
-}
+// class About extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <p>My name is Troy Conner Sipprelle.</p>
+//         <p>
+//           I am a Software Engineer for Booz Allen Hamilton, and I live in the
+//           Washington, D.C. Metro Area. I like eating, cooking, watching movies,
+//           and playing the ukulele.
+//         </p>
+//         <p>
+//           I graduated from Williams College in the spring of 2017 with a Math
+//           major and a focus on Architecture. Since then, I've learned to code
+//           and am excited about building both physical and less-tangible things
+//           that make people happy.
+//         </p>
+//         <p>
+//           Check out my: <a href="##">resume</a>
+//         </p>
+//       </div>
+//     );
+//   }
+// }
 
-export default About;
+// export default About;
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,53 +1,53 @@
-import React, { Component } from "react";
-import { Link, Route, Switch, Redirect } from "react-router-dom";
+// import React, { Component } from "react";
+// import { Link, Route, Switch, Redirect } from "react-router-dom";
 
-import "./App.css";
-import About from "./About";
-import Projects from "./Projects";
-import me from "../Images/me.jpeg";
+// import "./App.css";
+// import About from "./About";
+// import Projects from "./Projects";
+// import me from "../Images/me.jpeg";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">
-            <Link to="/">Troy Sipprelle</Link>
-          </h1>
-        </header>
-        <div className="body">
-          <nav>
-            {/* <p>Want to know...</p> */}
-            <Link to="/about">More about me</Link>
-            <Link to="/projects">What I've been up to lately</Link>
-          </nav>
-          <div className="main">
-            <Switch>
-              <Route exact path="/about" render={() => <About />} />
-              <Route exact path="/projects" render={() => <Projects />} />
-              <Route path="/*" render={() => <Redirect to="/" />} />
-            </Switch>
-          </div>
-          <img src={me} className="me-pic" />
-        </div>
-        <footer>
-          <a
-            className="footer-link"
-            href="https://www.linkedin.com/in/troy-sipprelle-516b1491/"
-          >
-            LinkedIn
-          </a>
-          <a className="footer-link" href="https://github.com/troysipp">
-            GitHub
-          </a>
-        </footer>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <h1 className="App-title">
+//             <Link to="/">Troy Sipprelle</Link>
+//           </h1>
+//         </header>
+//         <div className="body">
+//           <nav>
+//             {/* <p>Want to know...</p> */}
+//             <Link to="/about">More about me</Link>
+//             <Link to="/projects">What I've been up to lately</Link>
+//           </nav>
+//           <div className="main">
+//             <Switch>
+//               <Route exact path="/about" render={() => <About />} />
+//               <Route exact path="/projects" render={() => <Projects />} />
+//               <Route path="/*" render={() => <Redirect to="/" />} />
+//             </Switch>
+//           </div>
+//           <img src={me} className="me-pic" />
+//         </div>
+//         <footer>
+//           <a
+//             className="footer-link"
+//             href="https://www.linkedin.com/in/troy-sipprelle-516b1491/"
+//           >
+//             LinkedIn
+//           </a>
+//           <a className="footer-link" href="https://github.com/troysipp">
+//             GitHub
+//           </a>
+//         </footer>
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
